e8a949bb9919ad3f7a4ec1357cdd453600c269a8
leehuitao <708984962@qq.com>
2022-02-13 16:57:07 +0800

    添加循环链表定位增删打印的实现

--- AlgorithmLearning/AlgorithmLearning/AlgorithmLearning/AlgorithmLearning.cpp
+++ AlgorithmLearning/AlgorithmLearning/AlgorithmLearning/AlgorithmLearning.cpp
@@ -5,17 +5,21 @@
 #include "ReverseLink.h"
 #include "BubbleSort.h"
 #include "QuickSort.h"
+#include "CircularLink.h"
 int main()
 {
     vector<int> popVector{ 4,2,5,7,9,1 };
 
     //反转链表
+    cout << "-------------------ReverseLink---------------------------- " << endl;
     ReverseLink reverseLink;
     reverseLink.printLinkNode();
     reverseLink.printReverseLinkNode();
     //冒泡排序（从小到大）
+    cout << "-------------------bubble---------------------------- " << endl;
     BubbleSort();
     //快排
+    cout << "-------------------qucik---------------------------- " << endl;
     cout << "source " << endl;
     for (auto it : popVector) {
         cout << it;
@@ -26,5 +30,16 @@ int main()
     for (auto it : popVector) {
         cout << it;
     }
+    cout << "-------------------Circular---------------------------- " << endl;
+    CircularLink cLink;
+    cLink.printIndex(5);
+    cout << endl;
+    cLink.addNodeIndex(5,100);
+    cout << endl;
+    cLink.deleteNodeIndex(5);
+    cout << endl;
+    cLink.addNodeIndex(0, 999);
+    cout << endl;
+    cLink.deleteNodeIndex(0);
 }
 
--- AlgorithmLearning/AlgorithmLearning/AlgorithmLearning/CircularLink.h
+++ AlgorithmLearning/AlgorithmLearning/AlgorithmLearning/CircularLink.h
@@ -13,8 +13,21 @@ public:
 	void deleteNodeIndex(int index);
 private:
 	void init() { 
-
+		//初始化循环链表
+		root = new LinkNode;
+		auto temp = root;
+		for (int i = 0; i < 10; i++) {
+			LinkNode* child = new LinkNode;
+			child->data = i + 1;
+			root->next = child;
+			root = child;
+		}
+		root->next = temp;
+		root = temp;
 	}
+	void print(LinkNode*);
+private:
+	LinkNode* root;
 };
 
 CircularLink::CircularLink()
@@ -24,12 +37,76 @@ CircularLink::CircularLink()
 
 inline void CircularLink::printIndex(int index)
 {
+	auto temp = root;
+	for (size_t i = 0; i < index; i++)
+	{
+		temp = temp->next;
+	}
+	print(temp);
 }
 
 inline void CircularLink::addNodeIndex(int index, int data)
 {
+	auto temp = root;//缓存头指针
+	if (index <= 0) {
+		//找到最后一位
+		while (temp->next != root)
+		{
+			temp = temp->next;
+		}
+	}
+	else
+	{
+		for (size_t i = 0; i < index-1; i++)
+		{
+			temp = temp->next;
+		}
+	}
+	auto next = temp->next;
+	LinkNode* node = new LinkNode;
+	node->data = data;
+	node->next = next;
+	temp->next = node;
+	if (index <= 0) root = node;
+	print(root);
 }
 
 inline void CircularLink::deleteNodeIndex(int index)
 {
+	auto temp = root;//缓存头指针
+	if (index <= 0) {
+		//找到最后一位
+		while (temp->next != root)
+		{
+			temp = temp->next;
+		}
+		auto next = root;
+		root = root->next;
+		temp->next = root;
+		delete next;
+	}
+	else
+	{
+		for (size_t i = 0; i < index - 1; i++)
+		{
+			temp = temp->next;
+		}
+		auto next = temp->next;
+		temp->next = temp->next->next;
+		delete next;
+		next = nullptr;
+	}
+
+	print(root);
+}
+
+inline void CircularLink::print(LinkNode* node)
+{
+	auto tmp = node;
+	while (1)
+	{
+		cout << tmp->data;
+		if (tmp->next == node)return;
+		tmp = tmp->next;
+	}
 }
